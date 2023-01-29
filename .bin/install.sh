970ad89f738a61d519436508f6e96010aebf9129
Stetsed <stetsed@protonmail.com>
2023-01-29 13:08:49 +0100

    Updated install script to automatically install dotfiles

--- .bin/install.sh
+++ .bin/install.sh
@@ -15,5 +15,23 @@ install_package() {
   yay -Syu bluedevil bluez-utils brightnessctl grimblast-git neovim network-manager-applet rofi-lbonn-wayland-git sddm starship thunar-thunar-archive-plugin thunar-volman vscodium-bin webcord-bin wl-clipboard librewolf-bin chatgpt-desktop-bin
 }
 
-install_yay && install_package
+install_repository(){
+  git clone --bare git@github.com:Stetsed/.dotfiles.git $HOME/.dotfiles
+  function config {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+  }
+  mkdir -p .config-backup
+  config checkout
+  if [ $? = 0 ]; then
+    echo "Checked out config.";
+    else
+      echo "Backing up pre-existing dot files.";
+      config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+  fi;
+  config checkout
+  config config status.showUntrackedFiles no
+
+}
+
+install_yay && install_package && install_repository
 
