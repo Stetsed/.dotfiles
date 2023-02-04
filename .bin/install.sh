99b366eb938b16b90cd64ffe736dd76642c9ca1e
Stetsed <stetsed@protonmail.com>
2023-02-04 17:40:10 +0100

    Removed the backing up of files, gave a bunch of errors

--- .bin/install.sh
+++ .bin/install.sh
@@ -26,15 +26,7 @@ install_repository(){
   function config {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
   }
-  mkdir -p ~/.config-backup
-  config checkout
-  if [ $? = 0 ]; then
-    echo "Checked out config.";
-    else
-      echo "Backing up pre-existing dot files.";
-      config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-  fi;
-  config checkout
+  config checkout -f 
   config config status.showUntrackedFiles no
 }
 
