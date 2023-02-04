385c99c8eabafe30b6fd3b1e25267c2a2a69c6cd
Stetsed <stetsed@protonmail.com>
2023-02-04 19:39:31 +0100

    Added some extra configuration that is optional and just for me

--- .bin/install.sh
+++ .bin/install.sh
@@ -18,7 +18,7 @@ install_yay() {
 
 install_package() {
   # Install the package
-  yay -Syu --noconfirm gdm dunst bluedevil bluez-utils brightnessctl grimblast-git neovim network-manager-applet rofi-lbonn-wayland-git starship thunar thunar-archive-plugin thunar-volman vscodium-bin webcord-bin wl-clipboard librewolf-bin chatgpt-desktop-bin ttf-nerd-fonts-symbols-2048-em ttf-nerd-fonts-symbols-common neofetch swaybg waybar-hyprland-git
+  yay -Syu --noconfirm gdm dunst bluedevil bluez-utils brightnessctl grimblast-git neovim network-manager-applet rofi-lbonn-wayland-git starship thunar thunar-archive-plugin thunar-volman vscodium-bin webcord-bin wl-clipboard librewolf-bin chatgpt-desktop-bin ttf-nerd-fonts-symbols-2048-em ttf-nerd-fonts-symbols-common neofetch swaybg waybar-hyprland-git nfs-utils btop tldr swaylock-effects obsidian
 }
 
 install_repository(){
@@ -30,5 +30,19 @@ install_repository(){
   config config status.showUntrackedFiles no
 }
 
+extra_configuration() {
+  echo "192.168.1.11:/mnt/vault/data /mnt/data nfs defaults,_netdev 0 0" | sudo tee -a /etc/fstab
+  sudo mount -t nfs 192.168.1.11:/mnt/vault/data /mnt/data nfs defaults,_netdev 0 0
+  ln -s /mnt/data/storage ~/Storage
+  sudo systemctl enable --now bluetooth
+  sudo systemctl enable gdm
+
+}
+
 install_yay && install_package && install_repository
 
+if [[ $@ == *"--extra"* ]]; then
+  extra_configuration
+else
+  exit
+fi
