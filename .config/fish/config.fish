9f77cc46b0a45bed02dc4e6c2ce903f95e35a3e7
Stetsed <stetsed@protonmail.com>
2023-02-19 20:22:47 +0100

    Moved export to fish shell
    Learned that to export enviroment variables Hyprland uses the default
    shell to execute so for me that's shell so I moved the exports into
    there.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
-
 end
 
 set fish_greeting ""
+export QT_QPA_PLATFORM=wayland
+export MOZ_ENABLE_WAYLAND=1
+export TERM=xterm-256color
+export EDITOR=nvim
