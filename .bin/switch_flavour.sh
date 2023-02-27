e124d82a32ece820dc7fe1956d29b700e18168dc
Stetsed <stetsed@protonmail.com>
2023-02-27 11:40:40 +0100

    Cleaned up and created switching script

--- .bin/switch_flavour.sh
+++ .bin/switch_flavour.sh
@@ -4,18 +4,24 @@ switch_to_bluesky ()
 {
   cp ~/.config/alacritty/bluesky.alacritty.yml ~/.config/alacritty/alacritty.yml
   sed -i "s|exec = swaybg.*|exec = swaybg -m fill -i ~/.bin/bluesky_wallpaper.png|g" ~/.config/hypr/hyprland.conf
+  sed -i "s|image=.*|image=~/.bin/bluesky_wallpaper.png|g" ~/.config/swaylock/config
 }
 
 switch_to_darknight ()
 {
   cp ~/.config/alacritty/darknight.alacritty.yml ~/.config/alacritty/alacritty.yml
   sed -i "s|exec = swaybg.*|exec = swaybg -m fill -i ~/.bin/darknight_wallpaper.png|g" ~/.config/hypr/hyprland.conf
+  sed -i "s|image=.*|image=~/.bin/darknight_wallpaper.png|g" ~/.config/swaylock/config
 }
 
-if grep -q "0x010002" ~/.config/alacritty/alacritty.yml; then
+CHOICE=$(printf "Bluesky\nDarknight" | rofi -dmenu)
+
+if [ "$CHOICE" = "Bluesky" ]; then
     switch_to_bluesky
-elif grep -q "0x0e1c2f" ~/.config/alacritty/alacritty.yml ; then
+    notify-send "Switched to BlueSky"
+elif [ "$CHOICE" =  "Darknight" ]; then
     switch_to_darknight
+    notify-send "Switched to Darknight"
 else
    notify-send "Irregular Pattern Detected, Failed to switch" 
 fi
