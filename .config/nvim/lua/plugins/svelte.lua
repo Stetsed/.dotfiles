24016483b35b9ebdf163c03bbe9b73ff6ea13258
Stetsed <stetsed@protonmail.com>
2023-04-21 20:35:01 +0200

    Added Svelte Config

---
+++ .config/nvim/lua/plugins/svelte.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        svelte = {},
+      },
+    },
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        svelte = true,
+      },
+    },
+  },
+}
