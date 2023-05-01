171d84dd41fb72f699c467d01dff4157b603fdf7
Stetsed <stetsed@protonmail.com>
2023-05-01 12:49:58 +0200

    Updated nano function to output error if file does not exist

--- .config/fish/functions/nano.fish
+++ .config/fish/functions/nano.fish
@@ -1,5 +1,9 @@
 function nano --description 'Edit File with sudo if needed'
     set file $argv[1]
+    if test ! -e $file
+        echo "File $file does not exist"
+        return 1
+    end
     if test -w $file
         kitten edit-in-kitty $argv
     else
