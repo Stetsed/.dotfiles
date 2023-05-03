2502cb429d690d8059da0973d84c490903430039
Stetsed <stetsed@protonmail.com>
2023-05-03 16:29:06 +0200

    Fixed

--- .config/fish/functions/nano.fish
+++ .config/fish/functions/nano.fish
@@ -2,9 +2,10 @@ function nano --description 'Edit File with sudo if needed'
     set file $argv[1]
     if test ! -e $file
         touch $file
-    else if test -w $file
-        kitten edit-in-kitty $argv
-    else
-        sudo kitten edit-in-kitty $argv
+        if test -w $file
+            kitten edit-in-kitty $argv
+        else
+            sudo kitten edit-in-kitty $argv
+        end
     end
 end
