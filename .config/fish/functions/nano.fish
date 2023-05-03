3323442275f330b8bb891c55243c334b36eda631
Stetsed <stetsed@protonmail.com>
2023-05-03 16:25:08 +0200

    Made it touch if file is non-existant

--- .config/fish/functions/nano.fish
+++ .config/fish/functions/nano.fish
@@ -1,9 +1,7 @@
 function nano --description 'Edit File with sudo if needed'
     set file $argv[1]
-    if test (count $argv) = 0
-        set file ''
-    else if test ! -e $file
-        return 1
+    if test ! -e $file
+        touch $file
     else if test -w $file
         kitten edit-in-kitty $argv
     else
