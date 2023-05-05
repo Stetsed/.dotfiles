2f90766197aa75f10ba8faf5d481427d0a237014
Stetsed <stetsed@protonmail.com>
2023-05-05 11:53:29 +0200

    Moved back to old nano script, issue seems to be kitty upstream?

--- .config/fish/functions/nano.fish
+++ .config/fish/functions/nano.fish
@@ -1,32 +1,16 @@
 function nano --description 'Edit File with sudo if needed'
     set file $argv[1]
-    if test ! -f $file
+    if test ! -e $file
         if test -w $file
             touch $file
-            if set -q SSH_CLIENT || set -q SSH_TTY
-                kitten edit-in-kitty $argv
-            else
-                nvim $argv
-            end
-        else
-            sudo touch $file
-            if set -q SSH_CLIENT || set -q SSH_TTY
-                sudo kitten edit-in-kitty $argv
-            else
-                sudo nvim $argv
-            end
-        end
-    else if test -w $file
-        if set -q SSH_CLIENT || set -q SSH_TTY
             kitten edit-in-kitty $argv
         else
-            nvim $argv
-        end
-    else
-        if set -q SSH_CLIENT || set -q SSH_TTY
+            sudo touch $file
             sudo kitten edit-in-kitty $argv
-        else
-            sudo nvim $argv
         end
+    else if test -w $file
+        kitten edit-in-kitty $argv
+    else
+        sudo kitten edit-in-kitty $argv
     end
 end
