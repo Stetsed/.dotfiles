89c59c84ef166a526aa9bed9bd2f44962cc34254
Stetsed <stetsed@protonmail.com>
2023-05-05 18:22:56 +0200

    Finally got a working version of the nano script

--- .config/fish/functions/nano.fish
+++ .config/fish/functions/nano.fish
@@ -1,10 +1,27 @@
 function nano --description 'Edit File with sudo if needed'
+    # To anybody reading this function, I know it's fucking scuffed. Deal with it
     set file $argv[1]
+    set random_string (tr -dc A-Za-z0-9 </dev/urandom | head -c 13)
+
     if test ! -e $file
         echo "$file does not exist"
     else if test -w $file
         kitten edit-in-kitty $argv
     else
-        sudo kitten edit-in-kitty $argv
+        if set -q SSH_TTY; or set -q SSH_CLIENT
+            sudo kitten edit-in-kitty $argv
+        else
+            set tmp_dir "/tmp/$random_string"
+            set basename (basename $file)
+            set original_permissions (stat -c %a $file)
+            set original_owner (stat -c %u:%g $file)
+            mkdir -p $tmp_dir
+            cp $file $tmp_dir
+            kitten edit-in-kitty $tmp_dir/$basename
+            sudo cp $tmp_dir/$basename $file
+            sudo chmod $original_permissions $file
+            sudo chown $original_owner $file
+            sudo rm -rf $tmp_dir
+        end
     end
 end
