faeda4b30bbf66cd71e97068820db0bb21da0707
Stetsed <stetsed@protonmail.com>
2023-05-06 09:59:08 +0200

    switched to export for manpager cuz idfk

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,9 +6,9 @@ end
 set fish_greeting ""
 set QT_QPA_PLATFORM wayland
 set MOZ_ENABLE_WAYLAND 1
-set EDITORnvim
+set EDITOR nvim
 set LIBVIRT_DEFAULT_URI "qemu:///system"
-set MANPAGER "sh -c 'col -bx | bat -l man -p'"
+export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 set BAT_THEME Catppuccin-mocha
 set fzf_dir_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
 set PATH $PATH ~/.cargo/bin
