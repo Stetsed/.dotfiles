344d32e1f01f44a3c5fe6e68afbe477b8a8ab604
Stetsed <stetsed@protonmail.com>
2023-05-07 16:12:02 +0200

    Added ranger as paru file browser, and moved all exports to set -x

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,17 +3,17 @@ if status is-interactive
     starship init fish | source
 end
 
-set fish_greeting ""
-set QT_QPA_PLATFORM wayland
-set MOZ_ENABLE_WAYLAND 1
-set EDITOR nvim
-set LIBVIRT_DEFAULT_URI "qemu:///system"
-export MANPAGER="sh -c 'col -bx | bat -l man -p'"
-export FZF_DEFAULT_OPTS=" \
+set -x fish_greeting ""
+set -x QT_QPA_PLATFORM wayland
+set -x MOZ_ENABLE_WAYLAND 1
+set -x EDITOR nvim
+set -x LIBVIRT_DEFAULT_URI "qemu:///system"
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -x FZF_DEFAULT_OPTS " \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
-export BAT_THEME="Catppuccin-mocha"
-set fzf_dir_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
-set PATH $PATH ~/.cargo/bin
-set fzf_preview_dir_cmd exa --all --color=always
+set -x BAT_THEME Catppuccin-mocha
+set -x fzf_dir_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
+set -x PATH $PATH ~/.cargo/bin
+set -x fzf_preview_dir_cmd exa --all --color=always
