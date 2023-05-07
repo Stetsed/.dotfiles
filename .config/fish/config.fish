5d9bb6e534fac1967766aab4f5a27a6c9c416872
Stetsed <stetsed@protonmail.com>
2023-05-07 09:56:57 +0200

    Fixed Bat theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,7 +13,7 @@ export FZF_DEFAULT_OPTS=" \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
-set BAT_THEME Catppuccin-mocha
+export BAT_THEME="Catppuccin-mocha"
 set fzf_dir_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
 set PATH $PATH ~/.cargo/bin
 set fzf_preview_dir_cmd exa --all --color=always
