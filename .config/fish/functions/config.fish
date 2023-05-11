d2febec8e0be7130ec28af53a73ec8ca3b8f7372
Stetsed <stetsed@protonmail.com>
2023-05-11 10:51:52 +0200

    Fixed it creating a 0 file for no god dam reason

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,5 +1,5 @@
 function config --wraps='lazygit --git-dir=$HOME/.dotfiles --work-tree=$HOME' --description 'alias config=lazygit --git-dir=$HOME/.dotfiles --work-tree=$HOME'
-    if count $argv >0
+    if count $argv >/dev/null
         /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME $argv
     else
         lazygit --git-dir=$HOME/.dotfiles --work-tree=$HOME
