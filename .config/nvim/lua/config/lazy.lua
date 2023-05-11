eb0be4364b9de4b40eafe0a61947f8093a52bfcb
Stetsed <stetsed@protonmail.com>
2023-05-11 16:03:56 +0200

    Minor changes to neovim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -14,7 +14,7 @@ require("lazy").setup({
       import = "lazyvim.plugins",
     },
     { import = "lazyvim.plugins.extras.coding.copilot" },
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,5 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 return {
-  vim.o.clipboard == "unnamedplus",
+  vim.opt.completeopt == "menu, menuone, noselect, noinsert",
 }
