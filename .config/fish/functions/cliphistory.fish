18e8af16eaf9254423ff11ca45a281d4beb61d8a
Stetsed <stetsed@protonmail.com>
2023-05-13 12:51:38 +0200

    Added cliphist to allow for clipboard hisotyr

---
+++ .config/fish/functions/cliphistory.fish
@@ -0,0 +1,4 @@
+function cliphistory --wraps='cliphist list | fzf | cliphist decode | wl-copy' --description 'alias cliphistory=cliphist list | fzf | cliphist decode | wl-copy'
+  cliphist list | fzf | cliphist decode | wl-copy $argv
+        
+end
