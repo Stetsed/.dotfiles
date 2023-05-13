cc705f6b43d5de337863f4c22ab681bc5461aa73
Stetsed <stetsed@protonmail.com>
2023-05-13 14:21:32 +0200

    Cleaned up and added wallpaper script

---
+++ .bin/scripts/wallpaper.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+selection=$1
+
+result=$(echo $(swww query) | grep -o 'image: "[^"]*"')
+result=${result#image: \"}
+result=${result%\"}
+current_wallpaper=${result%\"}
+
+if [ "$selection" = "change" ]; then
+	if [ "$current_wallpaper" = "catppuccin_wallpaper.png" ]; then
+		new_wallpaper="Estradiol_non_bin.png"
+		swww img ~/.bin/backgrounds/pngs/$new_wallpaper --transition-type center
+	elif [ "$current_wallpaper" = "Estradiol_non_bin.png" ]; then
+		new_wallpaper="Estradiol_trans.png"
+		swww img ~/.bin/backgrounds/pngs/$new_wallpaper --transition-type center
+	elif [ "$current_wallpaper" = "Estradiol_trans.png" ]; then
+		new_wallpaper="catppuccin_wallpaper.png"
+		swww img ~/.bin/backgrounds/pngs/$new_wallpaper --transition-type center
+	fi
+fi
