eceee32c7ce0ef72dd008d66e0f33b16db80f379
Stetsed <stetsed@protonmail.com>
2023-05-16 10:34:50 +0200

    Removed unneeded kitty lines and made it so wl-copy doesnt hang when you try to exit

--- .config/fish/functions/cliphistory.fish
+++ .config/fish/functions/cliphistory.fish
@@ -1,4 +1,3 @@
 function cliphistory --wraps='cliphist list | fzf | cliphist decode | wl-copy' --description 'alias cliphistory=cliphist list | fzf | cliphist decode | wl-copy'
-  cliphist list | fzf | cliphist decode | wl-copy $argv
-        
+    cliphist list | fzf | cliphist decode | wl-copy $argv
 end
