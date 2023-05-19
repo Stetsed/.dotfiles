0b515490b7e1998c2eaed51e0ebfef0afb7c0fb7
Stetsed <stetsed@protonmail.com>
2023-05-19 09:54:14 +0200

    Removed the disable DPMS flag because well.. its worse for battery life

--- .bin/scripts/swayidle.sh
+++ .bin/scripts/swayidle.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 swayidle -w \
-        timeout 300 'swaylock' \
-        timeout 400 'hyprctl dispatch dpms off' \
-                resume 'hyprctl dispatch dpms on' \
-        before-sleep 'swaylock'
+	timeout 300 'swaylock' \
+	before-sleep 'swaylock'
