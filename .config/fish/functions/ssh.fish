90978f85511e2579f5e890bf2f9424dd0896815c
Stetsed <stetsed@protonmail.com>
2023-05-25 21:08:09 +0200

    Made it check if kitty is open to use kitty ssh

--- .config/fish/functions/ssh.fish
+++ .config/fish/functions/ssh.fish
@@ -1,4 +1,8 @@
 function ssh --description 'alias ssh=kitty +kitten ssh'
-  kitty +kitten ssh $argv
-        
+
+    if set -q $KITTY_PID
+        kitty +kitten ssh $argv
+    else
+        command ssh $argv
+    end
 end
