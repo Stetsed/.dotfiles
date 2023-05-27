6d78cdc8ea0daa89ba9f644d853d0c525336c0de
Stetsed <stetsed@protonmail.com>
2023-05-27 15:00:16 +0200

    Added path name so multiple paths can be uploaded

--- .bin/scripts/backup.sh
+++ .bin/scripts/backup.sh
@@ -10,11 +10,12 @@ if [[ hostname == "" ]]; then
 fi
 
 if [[ $1 == "--help" || $1 == "" ]]; then
-	echo "Usage: backup.sh backup [path] [--home] [--reset]"
+	echo "Usage: backup.sh backup [path] [--home|--root] [--reset]"
 	echo "Options:"
 	echo "Path: Pass the path to the volume you want to backup. Ex: zroot/data/home"
 	echo "--reset: Reset the backup and start from scratch"
 	echo "--home : Checks which volume is mounted to /home and backs that up"
+	echo "--root : Checks which volume is mounted to / and backs that up"
 	exit 0
 fi
 
@@ -25,11 +26,21 @@ if [[ $2 == "--home" ]]; then
 		echo "No Volume Mounted to /home"
 		exit 1
 	fi
+	path_name="home"
+elif [[ $2 == "--root" ]]; then
+	echo "Backing up /"
+	path=$(zfs list | grep '/$' | awk '{print $1}')
+	if [[ $path == "" ]]; then
+		echo "No Volume Mounted to /"
+		exit 1
+	fi
+	path_name="root"
 elif [[ $2 == "" ]]; then
 	echo "No Path Selected"
 	exit 1
 else
 	path=$2
+	path_name=$(echo $path | awk -F "/" '{print $NF}')
 fi
 
 if [[ $1 == "backup" ]]; then
@@ -45,7 +56,7 @@ if [[ $1 == "backup" ]]; then
 	else
 		time=$(date +%Y-%m-%d-%H-%M-%S)
 
-		sudo zfs snapshot -r $path@${time}-${hostname}
+		sudo zfs snapshot -r $path@${time}-${hostname}-${path_name}
 
 		path_exists=$(ssh truenas "zfs list -H -o name Vault/backups/${hostname}" | wc -l)
 
@@ -53,13 +64,13 @@ if [[ $1 == "backup" ]]; then
 			ssh truenas "zfs list -H -o name -t snapshot | grep "${hostname}" | xargs -n1 zfs destroy -r"
 			ssh truenas "zfs destroy -r Vault/backups/${hostname}"
 			trap "touch ~/.backup_interrupted.lock && exit " INT
-			sudo zfs send -vw $path@${time}-${hostname} | ssh truenas "zfs receive -Fs Vault/backups/${hostname}"
+			sudo zfs send -vw $path@${time}-${hostname} | ssh truenas "zfs receive -Fs Vault/backups/${hostname}-${path_name}"
 			notify-send "Backup Complete"
 			exit 0
 		else
 			trap "touch ~/.backup_interrupted.lock && exit " INT
 			old_snapshot=$(zfs list -t snapshot -o name | grep ${path} | awk '{y=z; z=$0} END{print y}')
-			sudo zfs send -vw -I ${old_snapshot} $path@${time}-${hostname} | ssh truenas "zfs receive -Fs Vault/backups/${hostname}"
+			sudo zfs send -vw -I ${old_snapshot} $path@${time}-${hostname} | ssh truenas "zfs receive -Fs Vault/backups/${hostname}-${path_name}"
 			notify-send "Backup Complete"
 			exit 0
 		fi
