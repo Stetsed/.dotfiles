b08813b751bec1e539545316c6ec13c50499ed6c
Stetsed <stetsed@protonmail.com>
2023-05-27 13:48:43 +0200

    Made you specify backup to run

--- .bin/scripts/backup.sh
+++ .bin/scripts/backup.sh
@@ -9,30 +9,43 @@ if [[ hostname == "" ]]; then
 	exit 1
 fi
 
-if [[ -f ~/.backup_interrupted.lock && $argv[1] != "--reset" ]]; then
-	# Get the last snapshot that was interrupted
-	echo "Running in Backup Interrupted Mode so trying to fetch resume token"
-	trap "touch ~/.backup_interrupted.lock && exit" INT
-	resume_token=$(ssh truenas "zfs get all Vault/backups/${hostname}" | grep receive | awk '{print $3}')
-	sudo zfs send -v -t "${resume_token}" | ssh truenas "zfs receive -F -s Vault/backups/${hostname}"
-	rm ~/.backup_interrupted.lock
-else
-	time=$(date +%Y-%m-%d-%H-%M-%S)
+if [[ $1 == "--help" ]]; then
+	echo "Usage: backup.sh backup [--reset]"
+	echo "Options:"
+	echo "--reset: Reset the backup and start from scratch"
+	exit 0
+fi
 
-	sudo zfs snapshot -r zroot/data/home@${time}-${hostname}
-
-	path_exists=$(ssh truenas "zfs list -H -o name Vault/backups/${hostname}" | wc -l)
-
-	if [[ $argv[1] == "--reset" || path_exists = 0 ]]; then
-		trap "touch ~/.backup_interrupted.lock && exit " INT
-		ssh truenas "zfs list -H -o name -t snapshot | grep "${hostname}" | xargs -n1 zfs destroy -r"
-		ssh truenas "zfs destroy -r Vault/backups/${hostname}"
-		sudo zfs send -vw zroot/data/home@${time}-${hostname} | ssh truenas "zfs receive -Fs Vault/backups/${hostname}"
-		notify-send "Backup Complete"
+if [[ $1 == "backup" ]]; then
+	echo "Backing up ${hostname}"
+	if [[ -f ~/.backup_interrupted.lock && $2 != "--reset" ]]; then
+		# Get the last snapshot that was interrupted
+		echo "Running in Backup Interrupted Mode so trying to fetch resume token"
+		trap "touch ~/.backup_interrupted.lock && exit" INT
+		resume_token=$(ssh truenas "zfs get all Vault/backups/${hostname}" | grep receive | awk '{print $3}')
+		sudo zfs send -v -t "${resume_token}" | ssh truenas "zfs receive -F -s Vault/backups/${hostname}"
+		rm ~/.backup_interrupted.lock
 	else
-		trap "touch ~/.backup_interrupted.lock && exit " INT
-		old_snapshot=$(zfs list -t snapshot -o name | awk '{y=z; z=$0} END{print y}')
-		sudo zfs send -vw -I ${old_snapshot} zroot/data/home@${time}-${hostname} | ssh truenas "zfs receive -Fs Vault/backups/${hostname}"
-		notify-send "Backup Complete"
+		time=$(date +%Y-%m-%d-%H-%M-%S)
+
+		sudo zfs snapshot -r zroot/data/home@${time}-${hostname}
+
+		path_exists=$(ssh truenas "zfs list -H -o name Vault/backups/${hostname}" | wc -l)
+
+		if [[ $2 == "--reset" || path_exists = 0 ]]; then
+			trap "touch ~/.backup_interrupted.lock && exit " INT
+			ssh truenas "zfs list -H -o name -t snapshot | grep "${hostname}" | xargs -n1 zfs destroy -r"
+			ssh truenas "zfs destroy -r Vault/backups/${hostname}"
+			sudo zfs send -vw zroot/data/home@${time}-${hostname} | ssh truenas "zfs receive -Fs Vault/backups/${hostname}"
+			notify-send "Backup Complete"
+		else
+			trap "touch ~/.backup_interrupted.lock && exit " INT
+			old_snapshot=$(zfs list -t snapshot -o name | awk '{y=z; z=$0} END{print y}')
+			sudo zfs send -vw -I ${old_snapshot} zroot/data/home@${time}-${hostname} | ssh truenas "zfs receive -Fs Vault/backups/${hostname}"
+			notify-send "Backup Complete"
+		fi
 	fi
+else
+	echo "Invalid option"
+	exit 1
 fi
