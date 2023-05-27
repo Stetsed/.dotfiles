8dcdba401e87236d8e23147cf32ca988e3814d57
Stetsed <stetsed@protonmail.com>
2023-05-27 13:43:51 +0200

    Automated checking if a previous exists

--- .bin/scripts/backup.sh
+++ .bin/scripts/backup.sh
@@ -12,31 +12,27 @@ fi
 if [ -f ~/.backup_interrupted.lock ]; then
 	# Get the last snapshot that was interrupted
 	echo "Running in Backup Interrupted Mode so trying to fetch resume token"
-	trap "touch ~/.backup_interrupted.lock && exit"
+	trap "touch ~/.backup_interrupted.lock && exit" INT
 	resume_token=$(ssh truenas "zfs get all Vault/backups/${hostname}" | grep receive | awk '{print $3}')
 	sudo zfs send -v -t "${resume_token}" | ssh truenas "zfs receive -F -s Vault/backups/${hostname}"
 	rm ~/.backup_interrupted.lock
 else
-	# Use the GUM TUI package to present an option to ask if this is the first snapshot for this system.
-	echo "Is this the first snapshot on this system? This will decide if it will send a delta or a whole snapshot"
-	first_snapshot=$(gum choose "Y" "N")
-
 	time=$(date +%Y-%m-%d-%H-%M-%S)
 
 	sudo zfs snapshot -r zroot/data/home@${time}-${hostname}
 
-	if [[ $first_snapshot == "Y" ]]; then
-		trap "touch ~/.backup_interrupted.lock && exit " INT EXIT
+	path_exists=$(ssh truenas "zfs list -H -o name Vault/backups/${hostname}" | wc -l)
+
+	if [[ $argv[1] == "--reset" || path_exists = 0 ]]; then
+		trap "touch ~/.backup_interrupted.lock && exit " INT
 		ssh truenas "zfs list -H -o name -t snapshot | grep "${hostname}" | xargs -n1 zfs destroy -r"
 		ssh truenas "zfs destroy -r Vault/backups/${hostname}"
 		sudo zfs send -vw zroot/data/home@${time}-${hostname} | ssh truenas "zfs receive -Fs Vault/backups/${hostname}"
 		notify-send "Backup Complete"
-	elif [[ $first_snapshot == "N" ]]; then
-		trap "touch ~/.backup_interrupted.lock && exit " INT EXIT
+	else
+		trap "touch ~/.backup_interrupted.lock && exit " INT
 		old_snapshot=$(zfs list -t snapshot -o name | awk '{y=z; z=$0} END{print y}')
 		sudo zfs send -vw -I ${old_snapshot} zroot/data/home@${time}-${hostname} | ssh truenas "zfs receive -Fs Vault/backups/${hostname}"
 		notify-send "Backup Complete"
-	else
-		echo 'Selection made wrong'
 	fi
 fi
