bc7138ec328db476b39a4cf5775e97e49462dfaa
Stetsed <stetsed@protonmail.com>
2023-05-28 13:30:27 +0200

    Made dpms off come sooner

--- .bin/scripts/swayidle.sh
+++ .bin/scripts/swayidle.sh
@@ -2,6 +2,6 @@
 
 swayidle -w \
 	timeout 300 '~/.bin/scripts/lock.sh' \
-	timeout 400 'hyprctl dispatch dpms off' \
+	timeout 350 'hyprctl dispatch dpms off' \
 	resume 'hyprctl dispatch dpms on' \
 	before-sleep '~/.bin/scripts/lock.sh'
