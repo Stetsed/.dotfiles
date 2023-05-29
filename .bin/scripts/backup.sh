4a3d068d006ffbd58ea20b10432c701ca9c1babe
Stetsed <stetsed@protonmail.com>
2023-05-29 14:34:16 +0200

    Wow

--- .bin/scripts/backup.sh
+++ .bin/scripts/backup.sh
@@ -80,7 +80,7 @@ if [[ $1 == "backup" ]]; then
 			exit 0
 		else
 			trap "touch ~/.backup_interrupted.lock && exit " INT
-			old_snapshot=$(zfs list -t snapshot -o name | grep ${path} | awk '{y=z; z=$0} END{print y}')
+			old_snapshot=$(zfs list -t snapshot -o name | grep ${path}@ | awk '{y=z; z=$0} END{print y}')
 			trap "zfs destroy $path@$time-$hostname-$path_name && exit" ERR
 			zfs send -vw -I ${old_snapshot} -R $path@${time}-${hostname}-${path_name} | ssh truenas "zfs receive -Fs Vault/backups/${hostname}-${path_name}"
 			notify-send "Backup Complete"
