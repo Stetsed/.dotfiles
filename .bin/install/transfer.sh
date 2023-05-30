947b23f935961f720ef1ab2d4c32e0832bda99f8
Stetsed <stetsed@protonmail.com>
2023-05-30 12:14:37 +0200

    Added some Gum Spinners

--- .bin/install/transfer.sh
+++ .bin/install/transfer.sh
@@ -12,35 +12,32 @@ File_Run() {
 
 File_Too() {
 	pkill librewolf
-	pkill webcord
-
-	cp -r ~/.librewolf ~/Network/Storage/Transfer/
+	gum spin -s dot --title "Copying Librewolf Files..." -- cp -r ~/.librewolf ~/Network/Storage/Transfer/
 
 	rm -rf ~/.config/WebCord/Cache/
+	pkill webcord
+	gum spin -s dot --title "Copying WebCord Files..." -- cp -r ~/.config/WebCord ~/Network/Storage/Transfer/
 
-	cp -r ~/.config/WebCord ~/Network/Storage/Transfer/
+	gum spin -s dot --title "Copying SSH Files..." -- cp -r ~/.ssh ~/Network/Storage/Transfer/
 
-	cp -r ~/.ssh ~/Network/Storage/Transfer/
-
-	cp -r ~/.env ~/Network/Storage/Transfer/
+	gum spin -s dot --title "Copying .env file..." -- cp -r ~/.env ~/Network/Storage/Transfer/
 	exit
 }
 
 File_From() {
 	pkill librewolf
-	pkill webcord
-
 	rm -rf ~/.librewolf
-	mv ~/Network/Storage/Transfer/.librewolf ~/
+	gum spin -s dot --title "Moving Librewolf Files..." -- mv ~/Network/Storage/Transfer/.librewolf ~/
 
+	pkill webcord
 	rm -rf ~/.config/WebCord
-	mv ~/Network/Storage/Transfer/WebCord ~/.config/
+	um spin -s dot --title "Moving WebCord Files..." -- mv ~/Network/Storage/Transfer/WebCord ~/.config/
 
 	rm -rf ~/.ssh
-	mv ~/Network/Storage/Transfer/.ssh ~/
+	um spin -s dot --title "Moving SSH Files..." -- mv ~/Network/Storage/Transfer/.ssh ~/
 
 	rm -rf ~/.env
-	mv ~/Network/Storage/Transfer/.env ~/
+	um spin -s dot --title "Moving .env file..." -- mv ~/Network/Storage/Transfer/.env ~/
 
 	gpg --import ~/Network/Storage/Long-Term/stetsed.asc
 }
