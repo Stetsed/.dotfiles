3146af2212b5418cfbb254c00adeae84aa8ec557
Stetsed <stetsed@protonmail.com>
2023-05-31 14:36:57 +0200

    Minor fixes

--- .bin/install/03-user_install.sh
+++ .bin/install/03-user_install.sh
@@ -12,14 +12,14 @@ User_Run() {
 
 	User_Extra
 
-	echo -n 'Are you Stetsed and do you wanna use Stetsed Specific Configuration for Storage: '
+	echo 'Are you Stetsed and do you wanna use Stetsed Specific Configuration for Storage: '
 	isstetsed=$(gum choose "Yes" "No")
 
 	if [[ $isstetsed == "Yes" ]]; then
 		User_Stetsed
 	fi
 
-	echo -n 'Program is Finished Executing, have a wonderful day :D'
+	echo 'Program is Finished Executing, have a wonderful day :D'
 	exit 0
 }
 
@@ -52,8 +52,8 @@ User_Paru() {
 }
 
 User_Dotfiles() {
-	echo -n 'Note: This installation expects your dotfiles to be in a bare repository, and expects there to be a .packages.list file in the root of the repository, which has 1 package per line.'
-	echo -n 'Enter the Github Repository you wanna use: '
+	echo 'Note: This installation expects your dotfiles to be in a bare repository, and expects there to be a .packages.list file in the root of the repository, which has 1 package per line.'
+	echo 'Enter the Github Repository you wanna use: '
 	repository=$(gum input --placeholder "stetsed/.dotfiles")
 
 	git clone --bare https://github.com/$repository.git $HOME/.dotfiles
@@ -75,12 +75,12 @@ User_Packages() {
 }
 
 User_Extra() {
-	echo -n 'To enable the bluetooth package you require the bluez package installed, and for pipewire you need pipewire and pipewire-pulse installed``'
+	echo 'To enable the bluetooth package you require the bluez package installed, and for pipewire you need pipewire and pipewire-pulse installed``'
 
-	echo -n 'Do you wanna install enable bluetooth?'
+	echo 'Do you wanna install enable bluetooth?'
 	bluetooth=$(gum choose "Yes" "No")
 
-	echo -n 'Do you want to enable pipewire?'
+	echo 'Do you want to enable pipewire?'
 	pipewire=$(gum choose "Yes" "No")
 
 	if [[ $bluetooth == "Yes" ]]; then
@@ -98,13 +98,13 @@ User_Extra() {
 	echo -e "[Service]\nExecStart=\nExecStart=-/usr/bin/agetty --autologin $username --noclear %I $TERM" | sudo tee -a /etc/systemd/system/getty@tty1.service.d/skip-prompt.conf
 	sudo systemctl enable getty@tty1.service
 
-	while [[ $time != "y" && $time != "n" ]]; do
-		echo -n 'Do you want to set the time to the correct timezone and enable timesyncd? (y/n): '
-		read time
+	while [[ $time != "Yes" && $time != "No" ]]; do
+		echo 'Do you want to set the time to the correct timezone and enable timesyncd? (y/n): '
+    time=$(gum choose "Yes" "No")
 	done
 
-	if [[ $time == "y" ]]; then
-		echo -n 'When entering timezone please use the following format: Region/City with correct capitalization.'
+	if [[ $time == "Yes" ]]; then
+		echo 'When entering timezone please use the following format: Region/City with correct capitalization.'
 		timezone=$(gum input --placeholder "Europe/Amsterdam")
 
 		sudo systemctl enable --now systemd-timesyncd.service
