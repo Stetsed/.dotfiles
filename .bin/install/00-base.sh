c5dec21adfb954cdfa48d808dec93655bad244f2
Stetsed <stetsed@protonmail.com>
2023-05-31 14:07:40 +0200

    Minor fix

--- .bin/install/00-base.sh
+++ .bin/install/00-base.sh
@@ -15,7 +15,7 @@ Main_Run() {
 	CHROOT="Chroot"
 	USER="User"
 	EXTRA="Extra"
-	SELECTION=$(gum choose --cursor-prefix "[ ] " --selected-prefix "[✓] " "$ZFS" "$CHROOT" "$USER" "$FILE_TRANSFER" "$EXTRA")
+	SELECTION=$(gum choose --cursor-prefix "[ ] " --selected-prefix "[✓] " "$ZFS" "$CHROOT" "$USER" "$EXTRA")
 	grep -q "$ZFS" <<<"$SELECTION" && bash -c "$(curl -Ls https://raw.githubusercontent.com/stetsed/.dotfiles/main/.bin/install/01-zfs_install.sh)"
 	grep -q "$CHROOT" <<<"$SELECTION" && bash -c "$(curl -Ls https://raw.githubusercontent.com/stetsed/.dotfiles/main/.bin/install/02-chroot_install.sh)"
 	grep -q "$USER" <<<"$SELECTION" && bash -c "$(curl -Ls https://raw.githubusercontent.com/stetsed/.dotfiles/main/.bin/install/03-user_install.sh)"
