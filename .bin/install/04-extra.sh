011d4f662c9c1c2bf08182c4286cb2f675333450
Stetsed <stetsed@protonmail.com>
2023-06-02 13:27:13 +0200

    Added sunshine setup

--- .bin/install/04-extra.sh
+++ .bin/install/04-extra.sh
@@ -7,13 +7,15 @@ Extra_Run() {
 	FRAMEWORK_80_100="Framework 80/100 Power Setup"
 	FRAMEWORK_FINGERPRINT="Framework Fingerprint Setup"
 	TRANSFER_FILES="Transfer Files"
-	SELECTION=$(gum choose "$SERVER_SETUP" "$ZFS_REMOTE_UNLOCK" "$FRAMEWORK_TLP" "$FRAMEWORK_80_100" "$FRAMEWORK_FINGERPRINT" "$TRANSFER_FILES")
+  SUNSHINE_SETUP="Setup Sunshine"
+	SELECTION=$(gum choose "$SERVER_SETUP" "$ZFS_REMOTE_UNLOCK" "$FRAMEWORK_TLP" "$FRAMEWORK_80_100" "$FRAMEWORK_FINGERPRINT" "$TRANSFER_FILES" "$SUNSHINE_SETUP")
 	grep -q "$SERVER_SETUP" <<<"$SELECTION" && Server_Setup
 	grep -q "$ZFS_REMOTE_UNLOCK" <<<"$SELECTION" && ZFS_Remote_Unlock_Setup
 	grep -q "$FRAMEWORK_TLP" <<<"$SELECTION" && Framework_TLP_Setup
 	grep -q "$FRAMEWORK_80_100" <<<"$SELECTION" && Framework_80_100_Setup
 	grep -q "$FRAMEWORK_FINGERPRINT" <<<"$SELECTION" && Framework_Fingerprint_Setup
 	grep -q "$TRANSFER_FILES" <<<"$SELECTION" && Transfer_Files
+  grep -q "$SUNSHINE_SETUP" <<<"$SELECTION" && Sunshine_Setup
 }
 
 Server_Setup() {
@@ -143,6 +145,18 @@ Transfer_Files_From() {
 	gum spin -s dot --title "Moving .env file..." -- mv ~/Network/Storage/Transfer/.env ~/
 
 	gpg --import ~/Network/Storage/Long-Term/stetsed.asc
+}
+
+Sunshine_Setup(){
+  paru -Syu sunshine
+
+  echo 'KERNEL=="uinput", SUBSYSTEM=="misc", OPTIONS+="static_node=uinput", TAG+="uaccess"' | sudo tee /etc/udev/rules.d/85-sunshine.rules
+
+  systemctl --user enable sunshine
+
+  sudo setcap cap_sys_admin+p $(readlink -f $(which sunshine))
+
+  echo "Sunshine Setup Complete, please reboot"
 }
 
 Extra_Run
