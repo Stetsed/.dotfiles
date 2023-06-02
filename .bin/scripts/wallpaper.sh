91328cea3f01fc017f8575226537683b1b567c31
Stetsed <stetsed@protonmail.com>
2023-06-02 15:11:48 +0200

    Added another wallpaper

--- .bin/scripts/wallpaper.sh
+++ .bin/scripts/wallpaper.sh
@@ -8,7 +8,7 @@ result=${result%\"}
 current_wallpaper=${result%\"}
 
 if [ "$selection" = "change" ]; then
-	if [ "$current_wallpaper" = "catppuccin_wallpaper.png" ]; then
+	if [ "$current_wallpaper" = "Catppuccin-Arch.png" ]; then
 		new_wallpaper="Estradiol_non_bin.png"
 		swww img ~/.bin/backgrounds/pngs/$new_wallpaper --transition-type center
 	elif [ "$current_wallpaper" = "Estradiol_non_bin.png" ]; then
@@ -17,6 +17,9 @@ if [ "$selection" = "change" ]; then
 	elif [ "$current_wallpaper" = "Estradiol_trans.png" ]; then
 		new_wallpaper="catppuccin_wallpaper.png"
 		swww img ~/.bin/backgrounds/pngs/$new_wallpaper --transition-type center
+  elif [[ "$current_wallpaper" == "catppuccin_wallpaper.png" ]]; then
+    new_wallpaper="Catppuccin-Arch.png"
+    swww img ~/.bin/backgrounds/pngs/$new_wallpaper --transition-type center
 	else
 		new_wallpaper="Estradiol_trans.png"
 		swww img ~/.bin/backgrounds/pngs/$new_wallpaper --transition-step 255
