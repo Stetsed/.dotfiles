433f3ce7b25be3bfafba1c6e3717f514afe5f08e
Stetsed <stetsed@protonmail.com>
2023-06-03 16:53:35 +0200

    Had to move opts to plugins folder

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -2,7 +2,8 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
@@ -13,14 +14,7 @@ require("lazy").setup({
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
     },
-    {
-      import = "lazyvim.plugins.extras.coding.copilot",
-      opts = {
-        suggestion = { enabled = true },
-        panel = { enabled = false },
-        fix_pairs = false,
-      },
-    },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.json" },
---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,8 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    suggestion = { enabled = true },
+    panel = { enabled = false },
+    fix_pairs = false,
+  },
+}
