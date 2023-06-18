8ba8f4c8bc350f038acc094094612a5396688b94
Stetsed <stetsed@protonmail.com>
2023-06-18 15:44:26 +0200

    Setting up yadm

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,7 +1,7 @@
 function config --wraps='lazygit --git-dir=$HOME/.dotfiles --work-tree=$HOME' --description 'alias config=lazygit --git-dir=$HOME/.dotfiles --work-tree=$HOME'
     if count $argv >/dev/null
-        /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME $argv
+        yadm $argv
     else
-        lazygit --git-dir=$HOME/.dotfiles --work-tree=$HOME
+        lazygit --git-dir=$HOME/.local/share/yadm/repo.git/ --work-tree=$HOME
     end
 end
