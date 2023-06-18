03d9642f216c229c8684ac05b60810a85e61179b
Stetsed <stetsed@protonmail.com>
2023-06-18 12:53:35 +0200

    Added some AI generated art and made it so I dont have to hardcode background names

--- .bin/scripts/wallpaper.sh
+++ .bin/scripts/wallpaper.sh
@@ -7,21 +7,43 @@ result=${result#image: \"}
 result=${result%\"}
 current_wallpaper=${result%\"}
 
-if [ "$selection" = "change" ]; then
-	if [ "$current_wallpaper" = "Catppuccin-Arch.png" ]; then
-		new_wallpaper="Estradiol_non_bin.png"
-		swww img ~/.bin/backgrounds/pngs/$new_wallpaper --transition-type center
-	elif [ "$current_wallpaper" = "Estradiol_non_bin.png" ]; then
-		new_wallpaper="Estradiol_trans.png"
-		swww img ~/.bin/backgrounds/pngs/$new_wallpaper --transition-type center
-	elif [ "$current_wallpaper" = "Estradiol_trans.png" ]; then
-		new_wallpaper="catppuccin_wallpaper.png"
-		swww img ~/.bin/backgrounds/pngs/$new_wallpaper --transition-type center
-  elif [[ "$current_wallpaper" == "catppuccin_wallpaper.png" ]]; then
-    new_wallpaper="Catppuccin-Arch.png"
-    swww img ~/.bin/backgrounds/pngs/$new_wallpaper --transition-type center
-	else
-		new_wallpaper="Estradiol_trans.png"
-		swww img ~/.bin/backgrounds/pngs/$new_wallpaper --transition-step 255
+directory="$HOME/.bin/backgrounds/pngs"
+
+# Step 1: Get the list of files in the directory
+files=(~/.bin/backgrounds/pngs/*)
+
+# Step 2: Sort the array of files
+sorted_files=($(printf '%s\n' "${files[@]}" | sort -V))
+
+# Step 3: Find the index of the given file name
+index=-1
+
+for i in "${!sorted_files[@]}"; do
+	if [[ "${sorted_files[$i]}" == "$directory/$current_wallpaper" ]]; then
+		index=$i
+		break
 	fi
+done
+
+# Step 4: Determine the next file
+next_index=$((index + 1))
+
+if [ $next_index -lt ${#sorted_files[@]} ]; then
+	next_file="${sorted_files[$next_index]}"
+	if [[ $next_file == *"falling_star.png"* ]]; then
+		next_index=$((next_index + 1))
+		if [ $next_index -lt ${#sorted_files[@]} ]; then
+			next_file="${sorted_files[$next_index]}"
+		else
+			next_file="${sorted_files[0]}"
+		fi
+	else
+		next_file="${sorted_files[$next_index]}"
+	fi
+else
+	next_file="${sorted_files[0]}" # Roll back to the first file
 fi
+
+echo "Setting wallpaper to $next_file"
+
+swww img "$next_file" -t center
