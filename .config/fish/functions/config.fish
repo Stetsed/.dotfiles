665b52c406187ddf3f589f992bd7436051114b95
Stetsed <stetsed@protonmail.com>
2023-06-20 09:05:30 +0200

    Updated config.fish to check if current directory is another git repo to avoid adding other stuff to the dotfiles

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,6 +1,10 @@
 function config --wraps='lazygit --git-dir=$HOME/.dotfiles --work-tree=$HOME' --description 'alias config=lazygit --git-dir=$HOME/.dotfiles --work-tree=$HOME'
     if count $argv >/dev/null
-        yadm $argv
+        if git rev-parse --is-inside-work-tree >/dev/null 2>&1
+            git $argv
+        else
+            yadm $argv
+        end
     else
         lazygit --git-dir=$HOME/.local/share/yadm/repo.git/ --work-tree=$HOME
     end
