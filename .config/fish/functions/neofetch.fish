3698c4b0cee8c6d9c8be4af4fbc1adaef3a80d10
Stetsed <stetsed@protonmail.com>
2023-06-21 09:29:21 +0200

    Switched from neofetch to treefetch, which brings it from 800ms per fetch run to 8ms

---
+++ .config/fish/functions/neofetch.fish
@@ -0,0 +1,4 @@
+function neofetch --wraps='~/.bin/scripts/binary/treefetch -b' --description 'alias neofetch=~/.bin/scripts/binary/treefetch -b'
+  ~/.bin/scripts/binary/treefetch -b $argv
+        
+end
