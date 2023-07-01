75b065b7c6fe63b5639d27b0d762c2a71a2e0519
Stetsed <stetsed@protonmail.com>
2023-07-01 15:26:43 +0200

    Switched from LibreWolf to Brave

--- .bin/install/04-extra.sh
+++ .bin/install/04-extra.sh
@@ -121,6 +121,9 @@ Transfer_Files_Too() {
 	pkill librewolf
 	gum spin -s dot --title "Copying Librewolf Files..." -- cp -r ~/.librewolf ~/Network/Storage/Transfer/
 
+	pkill brave
+	gum spin -s dot --title "Copying Brave Files..." -- cp -r ~/.config/BraveSoftware ~/Network/Storage/Transfer/
+
 	rm -rf ~/.config/WebCord/Cache/
 	pkill webcord
 	gum spin -s dot --title "Copying WebCord Files..." -- cp -r ~/.config/WebCord ~/Network/Storage/Transfer/
@@ -132,6 +135,9 @@ Transfer_Files_From() {
 	pkill librewolf
 	rm -rf ~/.librewolf
 	gum spin -s dot --title "Moving Librewolf Files..." -- mv ~/Network/Storage/Transfer/.librewolf ~/
+
+	pkill brave
+	gum spin -s dot --title "Moving brave files..." -- mv ~/Network/Storage/Transfer/BraveSoftware ~/.config/
 
 	pkill webcord
 	rm -rf ~/.config/WebCord
