603f2168f981454fc606355da628010134a88351
Stetsed <stetsed@selfhostable.net>
2023-08-15 13:45:42 +0200

    Minor cleanup

--- .config/fish/functions/benchmark.fish
+++
@@ -1,3 +0,0 @@
-function benchmark --wraps='hyperfine --runs 5 --warmup 5 --shell fish' --description 'alias benchmark=hyperfine --runs 5 --warmup 5 --shell fish'
-    hyperfine --runs 20 --warmup 5 --shell fish $argv
-end
