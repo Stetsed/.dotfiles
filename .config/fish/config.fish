58b5e8f41f1e043855903af1ae595892fdc2df1b
Stetsed <stetsed@selfhostable.net>
2024-01-24 13:09:22 +0100

    Poggers

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
+    atuin init fish --disable-up-arrow | source
 end
 
 set -x fish_greeting ""
