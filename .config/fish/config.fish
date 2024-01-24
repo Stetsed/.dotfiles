c39730b3756cb16b9d1599021eb26144ccf3d526
Stetsed <stetsed@selfhostable.net>
2024-01-24 13:27:05 +0100

    Guess no

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
-    atuin init fish --disable-up-arrow | source
 end
 
 set -x fish_greeting ""
