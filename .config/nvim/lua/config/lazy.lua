ed6e7f34e16546c4fa7207dbc476f892abe9593e
Stetsed <stetsed@selfhostable.net>
2025-01-07 15:09:31 +0100

    CLANG

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.ansible" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
 
     { import = "lazyvim.plugins.extras.dap.core" },
 
